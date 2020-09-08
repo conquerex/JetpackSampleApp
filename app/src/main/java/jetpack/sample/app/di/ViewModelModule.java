d638aabc33cd2a2bf4a60015227c1553e1ff4e11
Jongkook <conquerex@gmail.com>
2020-09-08 10:47:47 +0900

    게시 글 화면 구성하기 - ViewModel 추가

--- app/src/main/java/jetpack/sample/app/di/ViewModelModule.java
+++ app/src/main/java/jetpack/sample/app/di/ViewModelModule.java
@@ -1,9 +1,12 @@
 package jetpack.sample.app.di;
 
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
 import dagger.Binds;
 import dagger.Module;
+import dagger.multibindings.IntoMap;
+import jetpack.sample.app.ui.post.PostViewModel;
 
 /**
  * Created by jongkook on 2020.09.02
@@ -14,4 +17,10 @@ import dagger.Module;
 public abstract class ViewModelModule {
     @Binds
     abstract ViewModelProvider.Factory bindViewModelFactory(AppViewModelFactory factory);
+
+    // 오브젝트 그래프에 멀티 바인딩하도록 ViewModelModule에 다음과 같은 내용을 추가
+    @Binds
+    @IntoMap
+    @ViewModelKey(PostViewModel.class)
+    abstract ViewModel bindsPostViewModel(PostViewModel viewModel);
 }
--- app/src/main/java/jetpack/sample/app/ui/post/PostFragment.java
+++ app/src/main/java/jetpack/sample/app/ui/post/PostFragment.java
@@ -6,8 +6,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import javax.inject.Inject;
+
+import dagger.android.support.DaggerFragment;
 import jetpack.sample.app.R;
+import jetpack.sample.app.databinding.FragmentPostBinding;
+import jetpack.sample.app.di.AppViewModelFactory;
 
 /**
  * 현 시점에서는 Navigation Component에 Fragment가 등록되어 있지 않아서 에러가 발생
@@ -17,16 +23,33 @@ import jetpack.sample.app.R;
  * Binary XML file line #11: Binary XML file line #11:
  * Error inflating class androidx.fragment.app.FragmentContainerView
  */
-public class PostFragment extends Fragment {
+public class PostFragment extends DaggerFragment {
+
+    /**
+     * 오브젝트 그래프로부터 멤버 인젝션
+     */
+    @Inject
+    FragmentPostBinding binding;
+    @Inject
+    AppViewModelFactory viewModelFactory;
+
+    PostViewModel viewModel;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // ViewModel 객체를 요청
+        viewModel = new ViewModelProvider(this, viewModelFactory).get(PostViewModel.class);
+        if (savedInstanceState == null) {
+            // 데이터 요청, 프래그먼트가 재생성되었을 때는 요청하지 않는다.
+            viewModel.loadPosts();
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_post, container, false);
+//        return inflater.inflate(R.layout.fragment_post, container, false);
+        return binding.getRoot();
     }
 }
